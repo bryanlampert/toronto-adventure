86eec8db99ccd38416194ecabf17ffeea4468999
Bryan <bryanlampert@gmail.com>
2018-01-24 16:53:57 -0500

    Add background and ground sprite

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
-window.onload = function () {
-    let game = new Phaser.Game(960, 600, Phaser.AUTO, 'game');
+PlayState = {};
+
+PlayState.preload = function () {
+  this.game.load.json('level:1', 'data/level01.json');
+
+  this.game.load.image('background', 'images/background.png');
+  this.game.load.image('ground', 'images/ground.png');
 };
+
+PlayState.create = function () {
+  this.game.add.image(0, -150, 'background');
+  this._loadLevel(this.game.cache.getJSON('level:1'));
+};
+
+PlayState._loadLevel = function (data) {
+  data.platforms.forEach(this._spawnPlatform, this);
+};
+
+PlayState._spawnPlatform = function (platform) {
+  this.game.add.sprite(platform.x, platform.y, platform.image);
+};
+
+window.onload = function () {
+  let game = new Phaser.Game(960, 600, Phaser.AUTO, 'game');
+  game.state.add('play', PlayState);
+  game.state.start('play');
+};
+
